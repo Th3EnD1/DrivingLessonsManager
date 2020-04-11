e0ca19aeaca5bd12579d1e3e482e487a4971b0e8
Eden -Class <eden.moreno1@gmail.com>
2020-04-11 13:07:37 +0300

    Updated and Fixed ListOfLessons activity
    I made it like another project that only contains ListView so it may work now.

--- TASHTIT_ANDROID_PROJECT/ACTIVITIES/ListOfLessons.cs
+++ TASHTIT_ANDROID_PROJECT/ACTIVITIES/ListOfLessons.cs
@@ -22,19 +22,20 @@ namespace TASHTIT_ANDROID_PROJECT.ACTIVITIES
         //private EditText etLesson;
         //private ImageButton btnOk;
         //private ImageButton btnCancel;
-        private TextView txtHeader;
+        //private TextView txtHeader;
         private Button btnAddNewLesson;
 
         private Lessons lessons;
         private LessonsAdapter adapter;
 
-        int position = -1;
+        int position;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            lessons = lessons.SelectAll();
             SetContentView(Resource.Layout.ListOfLessons);
 
             SetViews();
@@ -43,12 +44,16 @@ namespace TASHTIT_ANDROID_PROJECT.ACTIVITIES
             //etLesson.Hint = "New Lesson";
             //etCity.InputType = Android.Text.InputTypes.ClassNumber;
 
-            lessons = new Lessons();
-            lessons = lessons.SelectAll();
+            //lessons = new Lessons();
+            //lessons = lessons.SelectAll();
+
+            adapter = new LessonsAdapter(this, Resource.Layout.OneItem, lessons);
+            lvLessons.Adapter = adapter;
 
             RefreshListView();
 
             Global.HideKeyboard(this, true);
+            position = -1;
         }
 
         private void SetViews()
@@ -57,7 +62,7 @@ namespace TASHTIT_ANDROID_PROJECT.ACTIVITIES
             //etLesson    = FindViewById<EditText>(Resource.Id.etLesson);
             //btnOk     = FindViewById<ImageButton>(Resource.Id.btnOk);
             //btnCancel = FindViewById<ImageButton>(Resource.Id.btnCancel);
-            txtHeader = FindViewById<TextView>(Resource.Id.txtHeader);
+            //txtHeader = FindViewById<TextView>(Resource.Id.txtHeader);
             btnAddNewLesson = FindViewById<Button>(Resource.Id.btnAddNewLesson);
 
             lvLessons.ItemClick += LvLessons_ItemClick;
@@ -125,74 +130,74 @@ namespace TASHTIT_ANDROID_PROJECT.ACTIVITIES
             diag.Show();
         }
 
-        private void BtnOk_Click(object sender, EventArgs e)
-        {
-            bool isNew = position == -1;
-            bool dataSetChanged = false;
+        //private void BtnOk_Click(object sender, EventArgs e)
+        //{
+        //    bool isNew = position == -1;
+        //    bool dataSetChanged = false;
 
-            Global.HideKeyboard(this);
+        //    Global.HideKeyboard(this);
 
-            //if (etLesson.Text != "")
-            //{
-            //    if (!etLesson.Text.StartsWith('0') || etLesson.Text.Length > 3)
-            //    {
-            //        Global.ToastCenteredText(this, "Area code should follow \n 1. Start with '0' \n 2. Maximum 3 digits", ToastLength.Long);
-            //    }
-                //else
-                Lesson lesson = new Lesson();
+        //    //if (etLesson.Text != "")
+        //    //{
+        //    //    if (!etLesson.Text.StartsWith('0') || etLesson.Text.Length > 3)
+        //    //    {
+        //    //        Global.ToastCenteredText(this, "Area code should follow \n 1. Start with '0' \n 2. Maximum 3 digits", ToastLength.Long);
+        //    //    }
+        //        //else
+        //        Lesson lesson = new Lesson();
         
-                if (isNew)
-                {
-                    if (!lessons.Exists(lesson))
-                    {
-                        lesson.EntityStatus = EntityStatus.ADDED;
-                        lessons.Add(lesson);
-                        dataSetChanged = true;
-                    }
-                }
-                else
-                {
-                    lesson.Id = lessons[position].Id;
-                    lesson.EntityStatus = lessons[position].EntityStatus;
+        //        if (isNew)
+        //        {
+        //            if (!lessons.Exists(lesson))
+        //            {
+        //                lesson.EntityStatus = EntityStatus.ADDED;
+        //                lessons.Add(lesson);
+        //                dataSetChanged = true;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            lesson.Id = lessons[position].Id;
+        //            lesson.EntityStatus = lessons[position].EntityStatus;
 
-                    if (lesson.Id != 0)
-                        lesson.EntityStatus = EntityStatus.MODIFIED;
+        //            if (lesson.Id != 0)
+        //                lesson.EntityStatus = EntityStatus.MODIFIED;
 
-                    if (!lessons.Exists(lesson, true))
-                    {
-                        lessons[position] = lesson;
-                        dataSetChanged = true;
-                    }
-                }
+        //            if (!lessons.Exists(lesson, true))
+        //            {
+        //                lessons[position] = lesson;
+        //                dataSetChanged = true;
+        //            }
+        //        }
 
-                if (dataSetChanged)
-                {
-                  //etLesson.Text = "";
-                    position = -1;
+        //        if (dataSetChanged)
+        //        {
+        //          //etLesson.Text = "";
+        //            position = -1;
 
-                    lessons.Sort();
+        //            lessons.Sort();
 
-                    RefreshListView();
-                }
-                else
-                {
-                    Android.Support.V7.App.AlertDialog.Builder alertDiag = new Android.Support.V7.App.AlertDialog.Builder(this);
+        //            RefreshListView();
+        //        }
+        //        else
+        //        {
+        //            Android.Support.V7.App.AlertDialog.Builder alertDiag = new Android.Support.V7.App.AlertDialog.Builder(this);
 
-                    alertDiag.SetTitle("Exists");
-                    alertDiag.SetMessage(lesson.Id + " already exists");
+        //            alertDiag.SetTitle("Exists");
+        //            alertDiag.SetMessage(lesson.Id + " already exists");
 
-                    alertDiag.SetCancelable(true);
+        //            alertDiag.SetCancelable(true);
 
-                    alertDiag.SetPositiveButton("OK", (senderAlert, args)
-                    =>
-                    {
-                        alertDiag.Dispose();
-                    });
+        //            alertDiag.SetPositiveButton("OK", (senderAlert, args)
+        //            =>
+        //            {
+        //                alertDiag.Dispose();
+        //            });
 
-                    Dialog diag = alertDiag.Create();
-                    diag.Show();
-                }
-        }
+        //            Dialog diag = alertDiag.Create();
+        //            diag.Show();
+        //        }
+        //}
 
         //private void BtnCancel_Click(object sender, EventArgs e)
         //{
