850913df6ac640e1954a5823246ac53d391a8c6c
Eden -Class <eden.moreno1@gmail.com>
2020-05-28 08:20:16 +0300

    AGAIN Updated Lesson Class (unnecessary feature)

--- MODEL/Lesson.cs
+++ MODEL/Lesson.cs
@@ -22,17 +22,15 @@ namespace MODEL
         private DateTime date;
         private DateTime time;
         private bool paid;
-        private string details;
         private double cost;
 
-        public Lesson(int studentNo, int lessonTypeNo, DateTime date, DateTime time, bool paid, string details, double cost)
+        public Lesson(int studentNo, int lessonTypeNo, DateTime date, DateTime time, bool paid, double cost)
         {
             this.studentNo = studentNo;
             this.lessonTypeNo = lessonTypeNo;
             this.date = date;
             this.time = time;
             this.paid = paid;
-            this.details = details;
             this.cost = cost;
         }
 
@@ -41,7 +39,6 @@ namespace MODEL
         public int LessonTypeNo { get => lessonTypeNo; set => lessonTypeNo = value; }
         public DateTime Date { get => date; set => date = value; }
         public bool Paid { get => paid; set => paid = value; }
-        public string Details { get => details; set => details = value; }
         public int StudentNo { get => studentNo; set => studentNo = value; }
         public DateTime Time { get => time; set => time = value; }
         public double Cost { get => cost; set => cost = value; }
@@ -55,7 +52,6 @@ namespace MODEL
                    date == lesson.date &&
                    time == lesson.time &&
                    paid == lesson.paid &&
-                   details == lesson.details &&
                    cost == lesson.cost;
         }
     }
